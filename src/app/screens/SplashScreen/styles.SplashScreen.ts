51ccdb8b0c3b9d5713edcbbd9107d64ab6521b77
gregorywoolery <gregorywoolery1@gmail.com>
2022-10-03 23:26:16 -0500

    reduce styles

--- src/app/screens/SplashScreen/styles.SplashScreen.ts
+++ src/app/screens/SplashScreen/styles.SplashScreen.ts
@@ -1,6 +1,9 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+    main_container: {
+        padding: 10
+    },
     header_container: {
         display: 'flex',
         justifyContent: "flex-end",
@@ -8,29 +11,22 @@ const styles = StyleSheet.create({
         width: '100%',
         paddingRight: 15
     },
-    splash_text: {
-        fontSize: 35,
-        fontWeight: "bold",
-        textAlign: 'center'
-    },
-    main_container: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: 10
-    },
-    main_text_container: {
-        display: 'flex',
-        alignContent: 'center',
-        marginTop: 40,
-        marginBottom: 20,
-        paddingRight: 20,
-        paddingLeft: 20
-    },
     content_container: {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
         height: '92%',
+    },
+    main_text_container: {
+        marginTop: 40,
+        marginBottom: 20,
+        paddingRight: 20,
+        paddingLeft: 20
+    },
+    splash_text: {
+        fontSize: 35,
+        fontWeight: "bold",
+        textAlign: 'center'
     },
     splash_button: {
         backgroundColor: '#48cf4d',
